72739b631bd663aabcb58ed9a7ca7126fd8828b8
Tristan <Tristandavis37@yahoo.ca>
2022-04-21 12:29:05 -0700

    Created function that allows to display single note when clicking contact from all contacts list

--- src/app.js
+++ src/app.js
@@ -54,20 +54,34 @@ const contactsList = [
 // Your code goes here
 
 const allContacts = document.getElementById("display_all_contacts")
+const singleCard = document.getElementById("display_single_contact")
 
-
+//load contacts
 
 function createContacts() {
   for (const contact of contactsList) {
     newDiv = document.createElement('div')
     contPara = document.createElement('p')
-    contPara.innerHTML = contact.name
+    conName = contact.name
+    conPhone = contact.phone
+    conEmail = contact.email
+    contPara.innerHTML = conName
     allContacts.appendChild(newDiv)
     newDiv.appendChild(contPara)
+    newDiv.addEventListener('click', displayContact)
   }
 }
-
 createContacts()
   
+//display single contact
 
+function displayContact(evt) {
+  contDiv = document.createElement('div')
+  contInfo = document.createElement('p')
+  contInfo.innerHTML = evt.target.innerHTML
+  singleCard.appendChild(contDiv)
+  contDiv.appendChild(contInfo)
+}
+
+//allContacts.addEventListener('click', displayContact)
 
